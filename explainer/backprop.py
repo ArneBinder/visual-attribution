8210ca854a295f182d3578024042d59e87dfae72
ArneBinder <ArneBinder@users.noreply.github.com>
2020-02-21 18:46:09 +0100

    multiple inputs and outputs

--- explainer/backprop.py
+++ explainer/backprop.py
@@ -4,88 +4,108 @@ import torch
 import types
 
 
+def default_backwards_function(output, index):
+    if index is None:
+        index = output.data.max(1)[1]
+
+    grad_out = output.data.clone()
+    grad_out.fill_(0.0)
+    grad_out.scatter_(1, index.unsqueeze(0).t(), 1.0)
+    output.backward(grad_out)
+
+
 class VanillaGradExplainer(object):
-    def __init__(self, model, explain_input_getter, explain_output_getter=None):
+    """
+     # Parameters
+
+    model: The `torch.nn.Module` to calculate explanations for.
+    grad_out_getter: function that takes the model output and an index and should call backward to produce gradients
+        at the inputs. See default_backwards_function for an example.
+
+    """
+    def __init__(self, model, backward_function=None):
         self.model = model
-        # explanations are calculated with respect to this input
-        self.explain_input_getter = explain_input_getter
-        self.explain_output_getter = explain_output_getter or (lambda x: x)
+        self.backward_function = backward_function or default_backwards_function
 
-    def _backprop(self, ind, **inputs):
-        self.input_explain = self.explain_input_getter(inputs, requires_grad=True)
-        #inputs[self.explain_input_setter] = Variable(inputs[self.explain_input_setter], requires_grad=True)
-        #input_explain = inputs[self.explain_input_setter]
+    def _backprop(self, args, kwargs, explain_index=None):
+        for arg in tuple(args) + tuple(kwargs.values()):
+            if isinstance(arg, torch.Tensor):
+                try:
+                    arg.requires_grad_()
+                except RuntimeError:
+                    pass
+        output = self.model.forward(*args, **kwargs)
+        self.backward_function(output=output, index=explain_index)
 
-        output_original = self.model(**inputs)
-        output = self.explain_output_getter(output_original)
+        #return inp.grad.data
+        return [arg.grad.data if isinstance(arg, torch.Tensor) and arg.grad is not None else None for i, arg in enumerate(args)], \
+               {arg_name: arg.grad.data for arg_name, arg in kwargs.items() if isinstance(arg, torch.Tensor) and arg.grad is not None}
 
-        grad_out = output.data.clone()
-        grad_out.fill_(0.0)
-        if ind is None:
-            ind = output.data.max(dim=-1)[1]
-            ind = ind.unsqueeze(-1)
-            grad_out.scatter_(-1, ind, 1.0)
-        else:
-            # if ind is a tuple, the second element is the token index
-            if isinstance(ind, tuple):
-                if len(ind) == 2:
-                    _grad_out = torch.zeros_like(output.data)
-                    _grad_out.scatter_(-1, ind[0].unsqueeze(-1), 1.0)
-                    grad_out[:, ind[1]] = _grad_out[:, ind[1]]
-                else:
-                    raise NotImplementedError('index with len(shape) > 2 not implemented')
-            else:
-                ind = ind.unsqueeze(-1)
-                grad_out.scatter_(-1, ind, 1.0)
+    def explain(self, *args, explain_index=None, **kwargs):
+        res = self._backprop(args, kwargs, explain_index)
+        return _select_result(*res)
 
-        output.backward(grad_out)
-        return self.input_explain.grad.data, output_original
 
-    def explain(self, ind=None, **inputs):
-        return self._backprop(ind, **inputs)
+def _prod(list_or_dict1, list_or_dict2):
+    res = {}
+    keys = list_or_dict1.keys() if isinstance(list_or_dict1, dict) else range(len(list_or_dict1))
+    for k in keys:
+        res[k] = list_or_dict1[k] * list_or_dict2[k]
+    return res
+
+
+def _select_result(args, kwargs):
+    if len(args) == 0 and len(kwargs) == 0:
+        return None
+    if len(args) == 0:
+        return kwargs
+    if len(kwargs) == 0:
+        return args
+    return args, kwargs
 
 
 class GradxInputExplainer(VanillaGradExplainer):
-    def __init__(self, model, **kwargs):
-        super(GradxInputExplainer, self).__init__(model, **kwargs)
+    def __init__(self, model, backward_function=None):
+        super(GradxInputExplainer, self).__init__(model, backward_function)
 
-    def explain(self, ind=None, **inputs):
-        grad, out = self._backprop(ind, **inputs)
-        return self.input_explain.data * grad, out
+    def explain(self, *args, explain_index=None, **kwargs):
+        arg_grads, kwarg_grads = self._backprop(args, kwargs, explain_index)
+        #return inp.data * grad
+        return _select_result(_prod(arg_grads, args), _prod(kwarg_grads, kwargs))
 
 
 class SaliencyExplainer(VanillaGradExplainer):
-    def __init__(self, model, **kwargs):
-        super(SaliencyExplainer, self).__init__(model, **kwargs)
+    def __init__(self, model, backward_function=None):
+        super(SaliencyExplainer, self).__init__(model, backward_function)
 
-    def explain(self, ind=None, **inputs):
-        grad, out = self._backprop(ind=ind, **inputs)
-        return grad.abs(), out
+    def explain(self, *args, explain_index=None, **kwargs):
+        arg_grads, kwarg_grads = self._backprop(args, kwargs, explain_index)
+        #return grad.abs()
+        return _select_result([v.abs() if v is not None else None for v in arg_grads], {k: v.abs() for k, v in kwarg_grads.items()})
 
 
 class IntegrateGradExplainer(VanillaGradExplainer):
-    def __init__(self, model, steps=100, **kwargs):
-        super(IntegrateGradExplainer, self).__init__(model, **kwargs)
+    def __init__(self, model, steps=100):
+        super(IntegrateGradExplainer, self).__init__(model)
+        raise NotImplementedError('IntegrateGradExplainer not yet adapted for multi input / output')
         self.steps = steps
 
-    def explain(self, ind=None, **inputs):
+    def explain(self, args, kwargs, explain_index=None):
         grad = 0
-        #inp_data = inputs[self.explain_input_setter].data.clone()
-        inp_data = self.explain_input_getter(inputs).data.clone()
+        inp_data = inp.data.clone()
 
         for alpha in np.arange(1 / self.steps, 1.0, 1 / self.steps):
             new_inp = Variable(inp_data * alpha, requires_grad=True)
-            inputs[self.explain_input_getter] = new_inp
-            g, out = self._backprop(ind=ind, **inputs)
+            g = self._backprop(new_inp, explain_index)
             grad += g
 
-        return grad * inp_data / self.steps, out
+        return grad * inp_data / self.steps
 
 
 class DeconvExplainer(VanillaGradExplainer):
     def __init__(self, model):
-        raise NotImplementedError('adapt changes from VanillaGradExplainer (see GradxInputExplainer)')
         super(DeconvExplainer, self).__init__(model)
+        raise NotImplementedError('DeconvExplainer not yet adapted for multi input / output')
         self._override_backward()
 
     def _override_backward(self):
@@ -112,8 +132,8 @@ class DeconvExplainer(VanillaGradExplainer):
 
 class GuidedBackpropExplainer(VanillaGradExplainer):
     def __init__(self, model):
-        raise NotImplementedError('adapt changes from VanillaGradExplainer (see GradxInputExplainer)')
         super(GuidedBackpropExplainer, self).__init__(model)
+        raise NotImplementedError('GuidedBackpropExplainer not yet adapted for multi input / output')
         self._override_backward()
 
     def _override_backward(self):
@@ -151,26 +171,22 @@ class SmoothGradExplainer(object):
         self.stdev_spread = stdev_spread
         self.nsamples = nsamples
         self.magnitude = magnitude
+        raise NotImplementedError('SmoothGradExplainer not yet adapted for multi input / output')
 
-    def explain(self,  ind=None, **inputs):
-        inp = inputs[self.base_explainer.explain_input]
+    def explain(self, inp, ind=None):
         stdev = self.stdev_spread * (inp.data.max() - inp.data.min())
 
         total_gradients = 0
         origin_inp_data = inp.data.clone()
 
         for i in range(self.nsamples):
-            noise = torch.randn(inp.size())
-            if torch.cuda.is_available():
-                noise = noise.cuda()
-            noise = noise * stdev
+            noise = torch.randn(inp.size()).cuda() * stdev
             inp.data.copy_(noise + origin_inp_data)
-            inputs[self.base_explainer.explain_input] = inp
-            grad, out = self.base_explainer.explain(ind=ind, **inputs)
+            grad = self.base_explainer.explain(inp, ind)
 
             if self.magnitude:
                 total_gradients += grad ** 2
             else:
                 total_gradients += grad
 
-        return total_gradients / self.nsamples, None
+        return total_gradients / self.nsamples
